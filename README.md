0bbd5fe54460031d6a71673e8fada706c2833915
Evan Greco <evan.greco@legitscript.com>
2019-08-18 13:15:18 -0500

    Clean up a bit, add blurb to README for usage

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# euler
+# Euler
+Currently contains the solution for Euler Project #2: https://projecteuler.net/problem=2
+
+# Usage
+Simply open `irb`, require `euler.rb`, create an `Euler::Fibonacci` object with a `max_term` in the constructor,
+and run `sum_even_fibonacci` on the object.  The result will be the answer to the problem.
+
+Example usage:
+```ruby
+require './euler.rb'
+
+fib = Euler::Fibonacci.new(4000000)
+puts "Result: #{fib.sum_even_fibonacci}"
+
+```
--- euler.rb
+++ euler.rb
@@ -1,31 +1,47 @@
-class Fibonacci
-  def initialize(max_term)
-    @max_term = max_term
-  end
+module Euler
+  class Fibonacci
+    def initialize(max_term)
+      @max_term = max_term
+    end
 
-  # Yields next even term in fibonacci sequence, up to @max_term
-  def fibonacci_even_only
-    return Enumerator.new { |generator|
-      first = 0
-      second = 1
+    # Yields next even term in fibonacci sequence, up to @max_term
+    def fibonacci_even_only
+      return Enumerator.new { |generator|
+        first = 0
+        second = 1
 
-      while true
-        if first == 0
-          generator.yield 0
+        while true
+          if first == 0
+            generator.yield 0
+          end
+
+          first, second = swap(first, first + second)
+          break if second >= @max_term
+          generator.yield(first) unless first % 2 != 0
         end
-        
-        first, second = swap(first, first + second)
-        break if second >= @max_term
-        generator.yield(first) unless first % 2 != 0
+      }
+    end
+
+    # Swaps first and second
+    def swap(first, second)
+      tmp = first
+      first = second
+      second = tmp
+      return first, second
+    end
+
+    # Returns sum of even fibonacci terms, with terms up to @max_term
+    def sum_even_fibonacci
+      generator = fibonacci_even_only
+      sum = 0
+      # I _swear_ there was generator.next? but I guess there totally isn't.  We break on StopIteration.
+      begin
+        while true
+        sum += generator.next
+        end
+      rescue StopIteration
+        return sum
       end
-    }
+    end
   end
-
-  def swap(first, second)
-    tmp = first
-    first = second
-    second = tmp
-    return first, second
-  end
-
 end
